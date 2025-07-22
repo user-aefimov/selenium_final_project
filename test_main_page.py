489e0f8372d484bb75d20759981a48ab6583f676
user-aefimov <user.a.efimov@gmail.com>
2025-07-22 14:37:43 +0300

    the first test PO

--- test_main_page.py
+++ test_main_page.py
@@ -2,25 +2,17 @@
 # Copyright (c) 2025 Aleksey Efimov
 # MIT License
 
-import pytest
+# import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser, language):
+def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    # Ожидание загрузки страницы
-    WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.ID, "login_link"))
-    )
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-    # Проверка языка интерфейса (дополнительная проверка)
-    html_lang = browser.find_element(By.TAG_NAME, "html").get_attribute("lang")
-    print(f"\nCurrent interface language: {html_lang}")
-    assert html_lang == language, f"Язык интерфейса не совпадает: {html_lang} != {language}"
 
